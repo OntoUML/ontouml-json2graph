46f4c5e4194058aea5c3494e3192e0bd03703fcf
pedropaulofb <pedropaulofb@gmail.com>
2023-08-01 23:07:41 +0200

    json2graph x tests structure working

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,11 @@
 """ Main test function. """
+import sys
 from pathlib import Path
 
 import pytest
 
-from main import decode_ontouml_json2graph
+sys.path.append('../json2graph')
+from json2graph.main import decode_ontouml_json2graph
 from test_aux import get_test_list, compare_graphs
 
 LIST_OF_TESTS = get_test_list()
