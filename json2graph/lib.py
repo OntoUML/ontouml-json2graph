0d0246660dcfaa63fa2203aa85c4a09e7077a18a
pedropaulofb <pedropaulofb@gmail.com>
2023-08-10 12:26:01 +0200

    structuration process

--- json2graph/lib.py
+++ json2graph/lib.py
@@ -1,10 +1,17 @@
 """ Functions to be used """
 
-# returns_graph
-# save_file
+from json2graph.decode import decode_ontouml_json2graph
+
+def decode_json_project(json_path):
+    pass
 
 
-def decode_json():
+def decode_json_model(json_path, base_uri, graph_format, language, model_only, silent, correct, execution_mode):
+    
+    decode_ontouml_json2graph(json_path, base_uri, graph_format, language, model_only, silent, correct, execution_mode)
+
     return None
-def decode_save_json()->None:
+
+
+def save_to_file(file_path:str) -> None:
     pass
