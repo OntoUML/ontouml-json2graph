613ae4009aa62c9ae8c3d71fc569657e5103e0ec
pedropaulofb <pedropaulofb@gmail.com>
2023-08-14 15:52:54 +0200

    update new version of validators included version update in remake.bat script

--- sphinx/remake.bat
+++ sphinx/remake.bat
@@ -1,1 +1,1 @@
-.\make clean && .\make html && rmdir ..\docs /s /q && mkdir ..\docs && xcopy .\_build\html ..\docs\ /E/H && make clean
+python ..\setup_metadata.py && .\make clean && .\make html && rmdir ..\docs /s /q && mkdir ..\docs && xcopy .\_build\html ..\docs\ /E/H && make clean
