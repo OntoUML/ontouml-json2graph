3d28ba7d7a8cdb40f3ca76b8e08e33f023ebfdd4
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-09-06 18:08:39 +0200

    fix library.py imports

--- json2graph/library.py
+++ json2graph/library.py
@@ -11,9 +11,9 @@ OntoUML graphs to files in their desired syntax.
 
 from rdflib import Graph
 
-from decode import decode_ontouml_json2graph
-from modules.errors import report_error_requirement_not_met
-from modules.input_output import safe_write_graph_file
+from .decode import decode_ontouml_json2graph
+from .modules.errors import report_error_requirement_not_met
+from .modules.input_output import safe_write_graph_file
 
 
 def decode_json_project(json_file_path: str,
