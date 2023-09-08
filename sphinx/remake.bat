6ed59e942443cd345cdbfbd206c31515c15b9084
Pedro Paulo Favato Barcelos <pedropaulofb@gmail.com>
2023-09-08 18:05:56 +0200

    sphinx docummentation updated

--- sphinx/remake.bat
+++ sphinx/remake.bat
@@ -1,1 +1,1 @@
-python ..\setup_metadata.py && .\make clean && .\make html && rmdir ..\docs /s /q && mkdir ..\docs && xcopy .\_build\html ..\docs\ /E/H && make clean
+echo "01/07: python ..\setup_metadata.py" && python ..\setup_metadata.py && echo "02/07: .\make clean "  && .\make clean && echo "03/07: .\make html" && .\make html && echo "04/07: rmdir ..\docs /s /q" && rmdir ..\docs /s /q && echo "05/07: mkdir ..\docs" && mkdir ..\docs && echo "06/07: xcopy .\_build\html ..\docs\ /E/H" && xcopy .\_build\html ..\docs\ /E/H && echo "07/07: make clean" && make clean
